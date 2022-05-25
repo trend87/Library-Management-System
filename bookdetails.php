01f484fc95bc4fad197a0687cb5d5b04ca93cb87
Effiong Israel Robert <israelrobert@gmail.com>
2022-05-25 12:53:09 +0100

    renamed style sheet and created bookdetails.php

---
+++ bookdetails.php
@@ -0,0 +1,12 @@
+<?php
+include 'config.php'
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include 'header.php';
+?>
+    <h3>About Books</h3>
+</body>
+</html>
--- books.php
+++ books.php
@@ -5,7 +5,7 @@
     
 
 // write query for all books
-$sql = 'SELECT cover, author, title, published FROM books ORDER BY published';
+$sql = 'SELECT cover, author, title, published, id FROM books ORDER BY published';
 
 //make query and get results
 $result = mysqli_query($conn, $sql);
@@ -29,17 +29,26 @@ $books = mysqli_fetch_all($result, MYSQLI_ASSOC);
     ?>
 <div class="container">
 <div class="row">
-        <?php foreach($books as $book):  ?>
-            <div class="card" >
+  
+
+
+    <?php foreach($books as $book):  ?>
+        <div class="col-lg-3 col-md-4">
+            <div class="card " >
             <img src="librarysystem.jpg" alt="Avatar" style="width:100%">
             <div class="container">
              <h4 ><?php echo htmlspecialchars($book['title']);?></h4> 
              <p ><strong><?php echo htmlspecialchars($book['author']);?></strong></p>
             <p >Published: <?php echo htmlspecialchars($book['published']);?></p>
+            <a href="bookdetails.php?id=<?php echo $book['id']; ?>">   <button type="button" class="btn btn-primary">More Info</button></a>
          </div>
         </div>
-
+        </div>
                 <?php endforeach; ?>
+
+            
+
+       
            
 
             
